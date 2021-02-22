13d5acf19f371c783f80c94971816b32216ce418
Maynard <elimaynard923@gmail.com>
2021-02-21 20:53:22 -0800

    make intercepts atomic

--- intercepts.py
+++ intercepts.py
@@ -37,7 +37,7 @@ def make_intercepts(flagset):
 
     def intercept(regex):
         def decorator(func):
-            intercepts.append(Intercept(re.compile(regex), func))
+            intercepts.append(Intercept(regex, func))
             return func
 
         return decorator
@@ -45,27 +45,35 @@ def make_intercepts(flagset):
     if flagset.twitter:
         """Allow modification of Twitter"""
 
-        @intercept("twitter.com")
-        def twitter(soup, flow, url_obj):
+        twitter_re = re.compile(r"twitter\.com")
 
-            if flagset.twitter_remove_home_feed:
-                """Remove the Home feed"""
+        if flagset.twitter_remove_home_feed:
+            """Remove the Home feed"""
+
+            @intercept(twitter_re)
+            def modify(soup, flow, url_obj):
                 remove(
                     '[aria-label="Timeline: Your Home Timeline"]',
                     from_=soup,
                     via="display-none",
                 )
 
-            if flagset.twitter_remove_trending:
-                """Remove the "What's happening" block"""
+        if flagset.twitter_remove_trending:
+            """Remove the "What's happening" block"""
+
+            @intercept(twitter_re)
+            def modify(soup, flow, url_obj):
                 remove(
                     '[aria-label="Timeline: Trending now"]',
                     from_=soup,
                     via="display-none",
                 )
 
-            if flagset.twitter_remove_follow_suggestions:
-                """Remove the "Who to follow" block"""
+        if flagset.twitter_remove_follow_suggestions:
+            """Remove the "Who to follow" block"""
+
+            @intercept(twitter_re)
+            def modify(soup, flow, url_obj):
                 remove(
                     '[aria-label="Who to follow"]',
                     from_=soup,
@@ -90,34 +98,51 @@ def make_intercepts(flagset):
         "thesffblog.com",
     ]
 
+    stackexchange_re = re.compile("|".join(map(re.escape, stackexchange_domains)))
+
     if flagset.stackexchange:
         """Allow modification of Stack Exchange websites"""
 
-        @intercept("|".join(map(re.escape, stackexchange_domains)))
-        def stackexchange(soup, flow, url_obj):
+        if flagset.stackexchange_remove_hot:
+            """Removes the "Hot Network Questions" sidebar"""
 
-            if flagset.stackexchange_remove_hot:
-                """Removes the "Hot Network Questions" sidebar"""
+            @intercept(stackexchange_re)
+            def modify(soup, flow, url_obj):
                 remove("#hot-network-questions", from_=soup, via="node-removal")
 
-            if flagset.stackexchange_remove_related:
-                """Removes the "Related" sidebar"""
+        if flagset.stackexchange_remove_related:
+            """Removes the "Related" sidebar"""
+
+            @intercept(stackexchange_re)
+            def modify(soup, flow, url_obj):
                 remove(".sidebar-related", from_=soup, via="node-removal")
 
-            if flagset.stackexchange_remove_rss_link:
-                """Removes the "Question feed" link"""
+        if flagset.stackexchange_remove_rss_link:
+            """Removes the "Question feed" link"""
+
+            @intercept(stackexchange_re)
+            def modify(soup, flow, url_obj):
                 remove("#feed-link", from_=soup, via="node-removal")
 
-            if flagset.stackexchange_remove_sticky_note:
-                """Removes the yellow "sticky note" on the right side of the page"""
+        if flagset.stackexchange_remove_sticky_note:
+            """Removes the yellow "sticky note" on the right side of the page"""
+
+            @intercept(stackexchange_re)
+            def modify(soup, flow, url_obj):
                 remove("#sidebar .s-sidebarwidget", from_=soup, via="node-removal")
 
-            if flagset.stackexchange_remove_left_sidebar:
-                """Removes the left navigation bar"""
+        if flagset.stackexchange_remove_left_sidebar:
+            """Removes the left navigation bar"""
+
+            @intercept(stackexchange_re)
+            def modify(soup, flow, url_obj):
                 remove("#left-sidebar", from_=soup, via="opacity-0")
 
-            if flagset.stackexchange_remove_se_landing_feed:
-                """Removes the feed on the landing page of stackexchange.com"""
+        if flagset.stackexchange_remove_se_landing_feed:
+            """Removes the feed on the landing page of stackexchange.com"""
+
+            @intercept(stackexchange_re)
+            def modify(soup, flow, url_obj):
                 is_parent_se_site = url_obj.netloc == "stackexchange.com"
                 if is_landing(url_obj) and not is_parent_se_site:
                     remove("#mainbar", from_=soup, via="node-removal")
@@ -125,15 +150,22 @@ def make_intercepts(flagset):
     if flagset.reddit:
         """Allow modification of Reddit"""
 
-        @intercept(r"(?<!old\.)reddit\.com")
-        def reddit_new(soup, flow, url_obj):
-            if is_landing(url_obj):
-                if flagset.reddit_remove_landing_feed:
-                    """Removes the feed from the homepage of Reddit"""
+        new_reddit_re = re.compile(r"(?<!old\.)reddit\.com")
+
+        if flagset.reddit_remove_landing_feed:
+            """Removes the feed from the homepage of Reddit"""
+
+            @intercept(new_reddit_re)
+            def modify(soup, flow, url_obj):
+                if is_landing(url_obj):
                     remove("html", from_=soup, via="node-removal")
-            else:
-                if flagset.reddit_remove_sub_feed:
-                    """Removes the feed from subreddits"""
+
+        if flagset.reddit_remove_sub_feed:
+            """Removes the feed from subreddits"""
+
+            @intercept(new_reddit_re)
+            def modify(soup, flow, url_obj):
+                if not is_landing(url_obj):
                     # Remove body from /r/{sub}
                     # Reddit seems to build the page content with JS, so we have to do this with CSS
                     if bool(re.match(r"/r/[^/?]+/?", url_obj.path)):
