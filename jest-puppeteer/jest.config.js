8ee8e0d74154e013bc1bb89f0b0077b81a938d6a
Maynard <elimaynard923@gmail.com>
2021-02-28 01:35:59 -0800

    jest, puppeteer, and a jest x puppeteer integration

---
+++ jest-puppeteer/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  rootDir: "../",
+  globalSetup: "./jest-puppeteer/setup.js",
+  globalTeardown: "./jest-puppeteer/teardown.js",
+  testEnvironment: "./jest-puppeteer/puppeteer_environment.js",
+};
