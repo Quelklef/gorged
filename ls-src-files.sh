793e4931c6dc819688aef296b999e7d7498b376b
Maynard <elimaynard923@gmail.com>
2021-03-08 19:43:59 -0800

    escape the '.' in '.git' in ls-src-files.sh

--- ls-src-files.sh
+++ ls-src-files.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 IFS=$'\n\t'
 
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-find -type f | { grep -vE '/(.git|venv|node_modules|__pycache__|chrome-profile)/' || [ $? = 1 ]; }
+find -type f | { grep -vE '/(\.git|venv|node_modules|__pycache__|chrome-profile)/' || [ $? = 1 ]; }
